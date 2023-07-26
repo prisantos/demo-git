c870b7d54f41d9cf6826f2f0d5d4eed90e9f8b9b
Priscilla Santos <priscilla.batista18@gmail.com>
2023-07-26 19:47:38 -0400

    [add] initial files

--- src/example.py
+++ src/example.py
@@ -1,2 +1,19 @@
 """Example"""
-print("Olá mundo!")
+
+
+def calculate_factorial(n):
+    """Calculate the factorial of a given integer.
+
+    This function takes an integer 'n' as input and calculates its factorial.
+
+    Parameters:
+        n (int): The integer for which the factorial needs to be calculated.
+
+    Returns:
+        int: The factorial of the input integer 'n'.
+
+    """
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i  # This is a long line of code that calculates the factorial of 'n'.
+    return factorial

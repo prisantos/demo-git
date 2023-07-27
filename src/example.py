b7d00cbfc3c87c93d93a8129f8951bd528a4ff03
Priscilla Santos <priscilla.batista18@gmail.com>
2023-07-27 14:30:38 -0400

    [add] initial files

--- src/example.py
+++ src/example.py
@@ -1,18 +1,2 @@
 """Example"""
-
-
-def calculate_circle_area(radius):
-    """Calculate the area of a circle.
-
-    This function takes the radius of a circle as input and calculates its area.
-
-    Parameters:
-        radius (float): The radius of the circle.
-
-    Returns:
-        float: The area of the circle.
-
-    """
-    pi = 3.14159265358979323846
-    area = pi * radius * radius  # This line calculates the area of the circle using the formula: area = π * radius^2.
-    return area
+print("Olá")
